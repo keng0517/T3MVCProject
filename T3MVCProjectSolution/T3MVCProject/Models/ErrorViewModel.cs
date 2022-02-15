1390f5ae2c494824c44afce69af9b727ac0ce052
unknown <keng051700@gmail.com>
2022-02-15 14:15:22 +0800

    first commit

---
+++ T3MVCProjectSolution/T3MVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T3MVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
