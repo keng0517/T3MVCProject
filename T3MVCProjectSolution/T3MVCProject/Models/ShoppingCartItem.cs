5053620fe71ecfcc8ca982239b8660c942fdb0f8
lipin <lipinclp@gmail.com>
2022-02-17 12:48:56 +0800

    added my models only
    product and shoppingcartitems. as im still working on the controllers, repos. didnt want to add the views as it might cause errors

---
+++ T3MVCProjectSolution/T3MVCProject/Models/ShoppingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace T3PersonalWkSpcApp.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int ShoppingCartId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+
+        public Product Product { get; set; }
+        //not sure want to store amt, as it would be calculated anyway
+        public double Amount { get; set; }  //per item
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+    }
+}
